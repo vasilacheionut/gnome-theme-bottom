632a20092301db03a58acd481eaa17e5afab714a
vasilacheionut <57985027+vasilacheionut@users.noreply.github.com>
2021-03-06 19:15:32 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,6 +16,9 @@ tar -xf Mojave-light.tar.xz -C .theme
 mkdir .icons
 tar -xf 01-McMojave-circle.tar.xz -C .icons
 
+mkdir pictures
+tar -xf Mojave-pictures.tar.xz -C pictures
+
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 40
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
