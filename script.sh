04d0783fbf021679e5300abe4883103cff4739df
vasilacheionut <57985027+vasilacheionut@users.noreply.github.com>
2021-03-06 21:19:37 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,11 @@
 sudo apt update
-sudo apt upgrade
 sudo apt install gnome-tweaks -y
 sudo apt install gnome-shell-extensions -y
 
-mkdir mojave
-mkdir mojave/Pictures
+mkdir Pictures
 
-tar -xf Mojave-pictures.tar.xz -C mojave/Pictures
-cp -R mojave/Pictures/ /home/$USER
+tar -xf Mojave-pictures.tar.xz -C Pictures
+cp -R Pictures/ /home/$USER
 
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 40
